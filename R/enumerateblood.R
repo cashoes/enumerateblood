e0d8301ca57011470206941670f389e5b547682a
Casey Shannon <also.casey@gmail.com>
2016-01-17 16:52:47 -0800

    Ran devtools::document().

---
+++ R/enumerateblood.R
@@ -0,0 +1,9 @@
+#' Enumerate cell types in blood from gene expression.
+#'
+#' A multi-response Gaussian model capable of accurately estimating
+#' the composition of blood samples from their gene expression profiles. Fit on
+#' Affymetrix Gene ST gene expression profiles using the glmnet R package.
+#'
+#' @format A model object of class cv.glmnet (implemented in the glmnet R package)
+#' @source \url{add citation}
+"enumerateblood"
